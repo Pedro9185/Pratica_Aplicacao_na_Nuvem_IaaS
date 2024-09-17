930558bce9730079594c8fd1a0fc4813273697dd
pedro9185 <pedrocatarina.ip@gmail.com>
2024-09-17 20:23:50 -0300

    first-commit

---
+++ init.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE employees;
+USE employees;
+
+
+CREATE TABLE employees (
+ firstName VARCHAR(50),
+ lastName VARCHAR(50),
+ email VARCHAR(100),
+ Title VARCHAR(50)
+);
+
+
+INSERT INTO employee_data
+ (firstName,lastName,email, Title)
+VALUES
+ ('Amit'  ,'Khanna','teste@gmail.com', 'Manager'),
+ ('Anjali' ,'Gupta','email2@teste.com', 'Engineer');
